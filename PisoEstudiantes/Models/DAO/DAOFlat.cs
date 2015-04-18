0c3746ec07e88ac216f2aa9e353ed0020c0342c5
Norman Coloma <ncg21@alu.ua.es>
2015-04-18 13:50:17 +0200

    Cambiada la definiciond el interfaz IFlat

---
+++ PisoEstudiantes/Models/DAO/DAOFlat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PisoEstudiantes.Models.DAO
+{
+    public class DAOFlat
+    {
+        private string bdConnection = ConfigurationManager.ConnectionStrings["CortinasDB"].ConnectionString;
+    }
+}
