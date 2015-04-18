fb1cd30cc8bd6413bc63f34c1c662fc10ca9e562
Norman Coloma <ncg21@alu.ua.es>
2015-04-18 20:14:36 +0200

    Creado el controlador del Home

---
+++ PisoEstudiantes/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using PisoEstudiantes.Models.BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PisoEstudiantes.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BOFlat flatModel = new BOFlat();
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            return View(flatModel.getLastFlats());
+        }
+    }
+}
