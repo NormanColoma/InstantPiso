98dbec82d1a45e857e1f6ca782973a0769dd6f93
Norman Coloma <ncg21@alu.ua.es>
2015-04-18 13:47:21 +0200

    Creado el CEN de Flat -> se encarga de la lógica de los pisos

---
+++ PisoEstudiantes/Models/BO/BOFlat.cs
@@ -0,0 +1,20 @@
+﻿using PisoEstudiantes.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PisoEstudiantes.Models.BO
+{
+    public class BOFlat : IFlat
+    {
+        /*public IEnumerable<Flat> getAllFlats()
+        {
+
+        } TODO*/
+        public IEnumerable<Flat> getFaltsByCity(string city)
+        {
+             
+        }
+    }
+}
