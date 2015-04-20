af2ea82101a230875ccc588d812c777ddde674c3
Norman Coloma <ncg21@alu.ua.es>
2015-04-20 18:55:46 +0200

    Más estilos añadidos

--- PisoEstudiantes/Controllers/AccountController.cs
+++ PisoEstudiantes/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using PisoEstudiantes.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,5 +15,34 @@ namespace PisoEstudiantes.Controllers
         {
             return View();
         }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            /*if (ModelState.IsValid)
+            {
+                var user = await UserManager.FindAsync(model.Email, model.Password);
+                if (user != null)
+                {
+                    await SignInAsync(user, model.RememberMe);
+                    return RedirectToLocal(returnUrl);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Nombre de usuario o contraseña no válidos.");
+                }
+            }*/
+
+            // Si llegamos a este punto, es que se ha producido un error y volvemos a mostrar el formulario
+            return View(model);
+        }
     }
+
+
+
+
 }
