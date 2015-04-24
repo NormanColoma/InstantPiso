13780feab287e8ee520c32b5034383f85b01700d
Norman <ua.norman@gmail.com>
2015-04-25 00:19:09 +0200

    Añadido vista perfil cuenta

---
+++ PisoEstudiantes/Models/AccountManageView.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PisoEstudiantes.Models
+{
+    public class AccountViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Correo electrónico")]
+        public string Email { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage = "Introduce un nombre válido")]
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage="Introduce un apellido válido")]
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Apellidos")]
+        public string Surname { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[9|6|7][0-9]{8}$", ErrorMessage="El teléfono introducido no es un teléfono válido")]
+        [Display(Name = "Teléfono")]
+        public string Phone { get; set; }
+    }
+}
