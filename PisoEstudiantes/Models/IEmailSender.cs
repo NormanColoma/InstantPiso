09ac0505b8500019ae35adbbbbcb2beca82fdf4e
Norman <ua.norman@gmail.com>
2015-04-29 21:12:06 +0200

    Gestor de email añadido:
    -Creado interfaz IEmailSender
    -Implementada entidad Email
    -Implementado interfaz en BOEmail
    -Añadidad funcionalidad de aviso de cierre de cuenta, tras el cierre de la misma

---
+++ PisoEstudiantes/Models/IEmailSender.cs
@@ -0,0 +1,17 @@
+﻿using PisoEstudiantes.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PisoEstudiantes.Models
+{
+    public interface IEmailSender
+    {
+        void sendEmail(User u);
+        void mannageCenter(User u, string action);
+        void accountCanceled(User u);
+        void userRegistered(User u);
+    }
+}
