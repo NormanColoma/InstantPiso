006319c79171dcb8fd15f0decc5cd12b8a227b3f
Norman <ua.norman@gmail.com>
2015-05-01 12:02:09 +0200

    Añadida la vista para el formulario de puesta en alquiler de pisos

---
+++ PisoEstudiantes/Controllers/PisosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PisoEstudiantes.Controllers
+{
+    public class PisosController : Controller
+    {
+        // GET: Pisos
+        public ActionResult Alquiler()
+        {
+            return View();
+        }
+    }
+}
