ad2e2b3e8ed06562339df14989e6355a4d71d005
Norman <ua.norman@gmail.com>
2015-05-03 12:40:03 +0200

    Preparando vista de gestión de pisos

---
+++ PisoEstudiantes/Controllers/API/PisosController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PisoEstudiantes.Controllers.API
+{
+    public class PisosController : ApiController
+    {
+
+    }
+}
