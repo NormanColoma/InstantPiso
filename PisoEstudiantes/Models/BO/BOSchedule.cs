8a00358a695b90e6af314f2cb6283389a191580a
Norman <ua.norman@gmail.com>
2015-05-15 14:16:16 +0200

    Añadido CEN Schedule

---
+++ PisoEstudiantes/Models/BO/BOSchedule.cs
@@ -0,0 +1,20 @@
+﻿using PisoEstudiantes.Models.DAO;
+using PisoEstudiantes.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PisoEstudiantes.Models.BO
+{
+    public class BOSchedule : ISchedule
+    {
+        private DAOSchedule ds = new DAOSchedule();
+        public bool createSchedule(Schedule s)
+        {
+            return ds.createSchedule(s);
+        }
+
+        
+    }
+}
