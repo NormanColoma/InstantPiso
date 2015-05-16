59b4757f015d9199ba9d72fd6939ac0f4a594cf5
Norman <ua.norman@gmail.com>
2015-05-16 14:45:54 +0200

    Implementado método read del CAD del Schedule

--- PisoEstudiantes/Models/DTO/Schedule.cs
+++ PisoEstudiantes/Models/DTO/Schedule.cs
@@ -7,59 +7,10 @@ namespace PisoEstudiantes.Models.DTO
 {
     public class Schedule
     {
-        /*private int id;
-        private int id_flat;
-        private string day;
-        private string hour;
-
-
-        public Schedule(int id, string day, string hour)
-        {
-            this.id = id;
-            this.day = day;
-            this.hour = hour;
-        }
-
-        public Schedule(string day, string hour)
-        {
-            this.day = day;
-            this.hour = hour;
-        }
-
-        public Schedule(Schedule s)
-        {
-            this.day = s.day;
-            this.hour = s.hour;
-            this.id = s.id;
-            this.id_flat = s.id_flat;
-        }
-
-        public int ID
-        {
-            get { return id; }
-            set { id = value; }
-        }
-
-        public int IDFlat
-        {
-            get { return id_flat; }
-            set { id_flat = value; }
-        }
-        public String Day
-        {
-            get { return day; }
-            set { day = value; }
-        }
-
-        public String Hour
-        {
-            get { return hour; }
-            set { hour = value; }
-        }*/
-
-
         public string Day { get; set; }
         public string Hour { get; set; }
         public int IDFlat { get; set; }
+
+        public int ID { get; set; }
     }
 }
