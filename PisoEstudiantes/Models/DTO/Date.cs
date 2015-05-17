9464a3ae611fcd17e506c2d932ba2a3dc9aef605
Norman <ua.norman@gmail.com>
2015-05-17 09:58:00 +0200

    Creada entidad Date

---
+++ PisoEstudiantes/Models/DTO/Date.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PisoEstudiantes.Models.DTO
+{
+    public class Date
+    {
+        public string Date { get; set; }
+        public int ID { get; set; }
+
+        public int IDFlat { get; set; }
+
+        public int IDOwner { get; set; }
+
+        public string UserEmail { get; set; }
+    }
+}
