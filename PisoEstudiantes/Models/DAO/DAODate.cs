21c992afa006eb6fa52e8c471a084614d705524c
Norman <ua.norman@gmail.com>
2015-05-17 10:11:03 +0200

    Agregado intefaz IDate y creado su CAD

---
+++ PisoEstudiantes/Models/DAO/DAODate.cs
@@ -0,0 +1,58 @@
+﻿using PisoEstudiantes.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace PisoEstudiantes.Models.DAO
+{
+    public class DAODate : IDate
+    {
+        private string bdConnection = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+        //Instancia de la conexión a la BD.
+        private SqlConnection conn = null;
+
+        //SqlCommand será el encargado de ejecutar la sentencia SQL.
+        private SqlCommand comm;
+
+        /*DataSet es un elemento donde almacenaremos los resultados de la consulta SQL, 
+        para posteriormente poder tratar los datos según nos convenga.*/
+        private DataSet ds = new DataSet();
+
+        //Adaptador que contendrá el DataSet.
+        private SqlDataAdapter da;
+
+        //DataTable es un elmento tipo tabla.
+        private DataTable t = new DataTable();
+
+        public bool createDate(Date date)
+        {
+            SqlConnection c = new SqlConnection(bdConnection);
+            try
+            {
+                c.Open();
+                SqlCommand comm = new SqlCommand("Insert Into [dbo].[Date](id_flat,date,owner,user) VALUES (@flat,@date,@ow,@us)", c);
+                comm.Parameters.AddWithValue("@flat", date.IDFlat);
+                comm.Parameters.AddWithValue("@date", date.BookingDate);
+                comm.Parameters.AddWithValue("@ow", date.IDOwner);
+                comm.Parameters.AddWithValue("@us", date.UserEmail);
+                int result = comm.ExecuteNonQuery();
+                if (result == 1)
+                    return true;
+                return false;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                c.Close();
+            }
+        }
+    }
+}
