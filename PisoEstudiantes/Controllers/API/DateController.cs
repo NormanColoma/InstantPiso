612d8d1ac770384e5eb1150e5f17a3c31de159ee
Norman <ua.norman@gmail.com>
2015-05-17 10:07:06 +0200

    Creado servicio rest para la reserva de cita

---
+++ PisoEstudiantes/Controllers/API/DateController.cs
@@ -0,0 +1,20 @@
+﻿using PisoEstudiantes.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PisoEstudiantes.Controllers.API
+{
+    public class DateController : ApiController
+    {
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult createDate([FromBody] Date date)
+        {
+            date.UserEmail = User.Identity.Name;
+            return Ok();
+        }
+    }
+}
