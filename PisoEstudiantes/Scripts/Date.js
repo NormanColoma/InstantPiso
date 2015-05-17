b506b6d4ca68dec121eff1f47fd2381e89262a13
Norman <ua.norman@gmail.com>
2015-05-17 09:57:55 +0200

    Creada entidad Date

--- PisoEstudiantes/Scripts/Date.js
+++ PisoEstudiantes/Scripts/Date.js
@@ -11,5 +11,21 @@ function createDate(date) {
     else
         $(".date-danger").hide();
 
-    var date = {}
+    var date = { Date: date, IDFlat: $(".f-id"), IDOwner: $(".f-owner") }
+    var port = location.port;
+    var uri = "http://localhost:" + port + "/api/Date";
+    var data = JSON.stringify(date);
+    $.ajax({
+        type: "POST",
+        url: uri,
+        dateType: "json",
+        contentType: "application/json",
+        data: data,
+        success: function (result) {
+           alert(result)
+        },
+        error: function () {
+            alert("mal")
+        }
+    });
 }
