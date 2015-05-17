0aa0f1ea9bc3e94d2e9ece2171513ab971afb91f
Norman <ua.norman@gmail.com>
2015-05-17 12:45:35 +0200

    Añadido interfaz INotification

---
+++ PisoEstudiantes/Models/INotifaction.cs
@@ -0,0 +1,15 @@
+﻿using PisoEstudiantes.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PisoEstudiantes.Models
+{
+    interface INotifaction
+    {
+        void createNotification(Notification notif);
+        void updateNotification(Notification notif);
+    }
+}
