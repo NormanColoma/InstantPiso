dee94c141d9799552d02dd8668e1aa3516f43689
Norman <ua.norman@gmail.com>
2015-05-17 09:52:12 +0200

    Añadida operación getOwnerID en el CAD user

---
+++ PisoEstudiantes/Scripts/Date.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $(".book-date").click(function () {
+        var date = $(".select option:selected").text();
+        createDate(date);
+    })
+})
+
+function createDate(date) {
+    if (date == "Selecciona la cita")
+        $(".date-danger").show();
+    else
+        $(".date-danger").hide();
+
+    var date = {}
+}
